2a3cd178469b64d92291bfb8b209684e21c7a9e2
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2021-01-22 11:56:53 -0600

    Initial Commit

---
+++ src/main/java/basic_rest_api_challenge/demorestapi/DemorestapiApplication.java
@@ -0,0 +1,13 @@
+package basic_rest_api_challenge.demorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemorestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/basic_rest_api_challenge/demorestapi/DemorestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package basic_rest_api_challenge.demorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemorestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
