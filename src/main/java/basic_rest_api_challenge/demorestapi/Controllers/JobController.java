7b54dcaf6a74a007f5166920e7b884baf4dd6067
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2021-01-28 11:23:15 -0600

    Updated Job Controller to use Job Service, Updated Job service with better documentation

--- src/main/java/basic_rest_api_challenge/demorestapi/Controllers/JobController.java
+++ src/main/java/basic_rest_api_challenge/demorestapi/Controllers/JobController.java
@@ -1,10 +1,15 @@
 package basic_rest_api_challenge.demorestapi.Controllers;
 
+import basic_rest_api_challenge.demorestapi.Exceptions.ResourceNotFoundException;
+import basic_rest_api_challenge.demorestapi.Models.Job;
 import basic_rest_api_challenge.demorestapi.Repos.JobRepository;
 import basic_rest_api_challenge.demorestapi.Services.ExceptionService;
+import basic_rest_api_challenge.demorestapi.Services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/a1")
@@ -16,5 +21,34 @@ public class JobController {
     @Autowired
     private ExceptionService exceptionService;
 
+    @Autowired
+    private JobService jobService;
 
+    //Find All Job Objects
+    @GetMapping("/jobs")
+    public List<Job> getAllJobs(){
+        return jobService.getAllJobs();
+    }
+
+    //Find Job Object by ID
+    @GetMapping("/job/{id}")
+    public Job getJobByID(@PathVariable(value = "id") long jobID) throws ResourceNotFoundException {
+        exceptionService.isJobNotNull(jobID);
+        return jobService.getJobByID(jobID);
+    }
+
+    //Create a New Job Object
+    @PostMapping("/newJob")
+    public Job createNewJob(@RequestBody Job job){
+        return jobService.addJob(job);
+    }
+
+    //Update a Job Object by ID
+    @PutMapping("/job/{id}")
+    public ResponseEntity<Job> updateJob(@PathVariable(value = "id") long jobID, @RequestBody Job jobDetails) throws ResourceNotFoundException{
+        Job jobToUpdate = jobService.getJobByID(jobID);
+        exceptionService.isJobNotNull(jobToUpdate);
+        jobService.updateJob(jobToUpdate, jobDetails.getJobTitle(), jobDetails.getSalary());
+        return ResponseEntity.ok().body(jobToUpdate);
+    }
 }
--- src/main/java/basic_rest_api_challenge/demorestapi/Services/JobService.java
+++ src/main/java/basic_rest_api_challenge/demorestapi/Services/JobService.java
@@ -11,53 +11,72 @@ import java.util.List;
 public class JobService {
     @Autowired
     private JobRepository jobDao;
-
+    //Adding Job Object
     public Job addJob(Job jobToAdd){
         System.out.println("Saving Job Object: " + jobToAdd);
         return jobDao.save(jobToAdd);
     }
 
+    //Finding Job Object by jobID
     public Job getJobByID(long jobID){
         System.out.println("Finding job with ID: " + jobID);
         return jobDao.findById(jobID);
     }
 
+    //Finding All Jobs
     public List<Job> getAllJobs(){
         System.out.println("Retrieving all job objects");
         return jobDao.findAll();
     }
 
-    public Job updateJob(long jobID, String newJobName){
-        Job jobToUpdate = getJobByID(jobID);
+    //Update Job Name and Salary
+    public Job updateJob(Job jobToUpdate, String newJobName, double newSalary){
         jobToUpdate.setJobTitle(newJobName);
+        jobToUpdate.setSalary(newSalary);
         return jobDao.save(jobToUpdate);
     }
 
+    //Update Job Name and Salary, Find Job by jobID
+    public Job updateJob(long jobID, String newJobName, double newSalary){
+        Job jobToUpdate = getJobByID(jobID);
+        return updateJob(jobToUpdate, newJobName, newSalary);
+    }
+
+    //Update Job Name
     public Job updateJob(Job jobToUpdate, String newJobName){
         jobToUpdate.setJobTitle(newJobName);
         return jobDao.save(jobToUpdate);
     }
 
-    public Job updateJob(long jobID, double newSalary){
+    //Update Job Name, Find Job by jobID
+    public Job updateJob(long jobID, String newJobName){
         Job jobToUpdate = getJobByID(jobID);
-        jobToUpdate.setSalary(newSalary);
-        return jobDao.save(jobToUpdate);
+        return updateJob(jobToUpdate, newJobName);
     }
 
+
+    //Update Job Salary
     public Job updateJob(Job jobToUpdate, double newSalary){
         jobToUpdate.setSalary(newSalary);
         return jobDao.save(jobToUpdate);
     }
 
+    //Update Job Salary, Find Job by jobID
+    public Job updateJob(long jobID, double newSalary){
+        Job jobToUpdate = getJobByID(jobID);
+        return updateJob(jobToUpdate, newSalary);
+    }
+
+    //Delete Job Object
     public void deleteJob(Job jobToDelete){
         System.out.println("Removing Job Object: " + jobToDelete);
         jobDao.delete(jobToDelete);
     }
 
+    //Delete Job Object, Find Job by jobID
     public void deleteJob(long jobID){
         Job jobToDelete = getJobByID(jobID);
-        System.out.println("Removing Job Object: " + jobToDelete);
-        jobDao.delete(jobToDelete);
+        deleteJob(jobToDelete);
     }
 
 }
